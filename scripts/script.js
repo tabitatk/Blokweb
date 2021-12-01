4800c6f1399838459804f625b19cbee088a8a64d
tabitatk <95043139+tabitatk@users.noreply.github.com>
2021-12-01 11:17:25 +0100

    nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+
+deButton = document.querySelector("header button");
+
+
+
+
+
+
+deButton.addEventListener("click", toggleMenu);
+
+
+
+
+
+function toggleMenu(event) {
+    deNav = event.target.parentNode;
+    deNav.classList.toggle("toonMenu");
+}
